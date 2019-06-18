d78fc89707d168e7eb5c5b4205d1ac7e6b1a1095
Leonardo Hernández <lhernandez@lumbrera.com.mx>
2019-06-18 12:36:57 -0500

    Add hex_age function

--- hello.py
+++ hello.py
@@ -1,5 +1,14 @@
 def hello(name):
 	print(f"¡Hello, {name}!")
 
+
 def add_age(age):
 	print(f"Age, {age}")
+
+def hello_french(name):
+        print(f"Bonjour, comment allez-vous, {name}")
+
+
+def hex_age(age):
+    return hex(age)
+
